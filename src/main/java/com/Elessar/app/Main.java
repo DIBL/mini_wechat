7cd13f372bbd3e83bf541b5ad4b217bab629d599
Jemma Fan <fanjinzhen1@gmail.com>
2019-01-07 22:59:42 -0800

    new

---
+++ src/main/java/com/Elessar/app/Main.java
@@ -0,0 +1,10 @@
+package com.Elessar.app;
+
+/**
+ * Created by Hans on 1/7/19.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
