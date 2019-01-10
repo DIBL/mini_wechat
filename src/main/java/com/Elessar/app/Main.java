22c5056a0ef2f54191f44e7f214ff83627acfc2a
Elessar <4529231+DIBL@users.noreply.github.com>
2019-01-10 09:03:33 -0800

    delete target/ and .idea/, move server code into a separate package and class in Main.java

--- src/main/java/com/Elessar/app/Main.java
+++ src/main/java/com/Elessar/app/Main.java
@@ -13,9 +13,24 @@ import java.util.Set;
 /**
  * Created by Hans on 1/7/19.
  */
+
 public class Main {
     public static void main(String[] args){
-        int port = 9000;
+        final MyServer server = new MyServer("localhost", 9000);
+        server.run();
+    }
+}
+
+class MyServer {
+    String serverName;
+    int port;
+
+    public MyServer(String serverName, int port) {
+        this.serverName = serverName;
+        this.port = port;
+    }
+
+    public void run() {
         HttpServer server = null;
         try {
             server = HttpServer.create(new InetSocketAddress(port), 0);
