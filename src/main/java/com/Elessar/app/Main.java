befe757f864c2f3760a7c63cc90e97c50f6666c3
Elessar <4529231+DIBL@users.noreply.github.com>
2019-01-09 23:22:37 -0800

    pull_request

--- src/main/java/com/Elessar/app/Main.java
+++ src/main/java/com/Elessar/app/Main.java
@@ -1,10 +1,62 @@
 package com.Elessar.app;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Hans on 1/7/19.
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int port = 9000;
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            System.out.println("Cannot create server at port");
+        }
+        System.out.println("server started at " + port);
+        server.createContext("/", new RootHandler());
+        server.createContext("/echo", new EchoHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+}
+
+class RootHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        String response = "<h1>Server start success if you see this message</h1>" + "<h1>Port: " + 9000 + "</h1>";
+        he.sendResponseHeaders(200, response.length());     // 200 means the request was successfully received
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
+
+class EchoHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        Headers headers = he.getRequestHeaders();
+        Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
+        String request = "";
+        for (Map.Entry<String, List<String>> entry : entries)
+            request += entry.toString() + "\n";
+        System.out.println(request);
+
+        String response = "OK";
+        he.sendResponseHeaders(200, response.length());
+        OutputStream os = he.getResponseBody();
+        os.write(response.toString().getBytes());
+        os.close();
     }
 }
