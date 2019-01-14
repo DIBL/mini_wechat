2cecbe3e08210a7378b898ec0925f60fb63112ac
Elessar <4529231+DIBL@users.noreply.github.com>
2019-01-13 16:13:53 -0800

    remove outter class in addressbook.proto

--- src/main/java/com/Elessar/app/Main.java
+++ src/main/java/com/Elessar/app/Main.java
@@ -1,10 +1,14 @@
 package com.Elessar.app;
 
 import com.Elessar.app.server.MyServer;
-import com.example.tutorial.AddressBookProtos;
-import com.example.tutorial.AddressBookProtos.Person;
 
-import java.io.*;
+import com.example.tutorial.Addressbook.Person;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 /**
  * Created by Hans on 1/7/19.
@@ -15,18 +19,17 @@ public class Main {
         final MyServer server = new MyServer("localhost", 9000);
         server.run();
         testProtoBuf();
-
     }
 
     private static void testProtoBuf() {
-        Person.Builder sendPersonData = Person.newBuilder().setId(123).setName("DIBL").setEmail("abc@gmail.com");
-        Person.PhoneNumber.Builder phoneNumber = Person.PhoneNumber.newBuilder().setNumber("321-7684231");
+        final Person.Builder sendPersonData = Person.newBuilder().setId(123).setName("DIBL").setEmail("abc@gmail.com");
+        final Person.PhoneNumber.Builder phoneNumber = Person.PhoneNumber.newBuilder().setNumber("321-7684231");
         sendPersonData.addPhones(phoneNumber);
 
         try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
             sendPersonData.build().writeTo(os);
             try (InputStream is = new ByteArrayInputStream(os.toByteArray())) {
-                Person receivePersonData = Person.parseFrom(is);
+                final Person receivePersonData = Person.parseFrom(is);
                 System.out.printf("receive and send data is equal: %b\n", sendPersonData.equals(receivePersonData));
                 System.out.println(receivePersonData.toString());
                 System.out.println(sendPersonData.toString());
